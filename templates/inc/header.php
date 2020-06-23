4732555305b3da638bb870b9416a4d625bac0751
Qamar30 <you@example.com>
2020-06-24 01:58:12 +0200

    navbar

--- templates/inc/header.php
+++ templates/inc/header.php
@@ -2,23 +2,22 @@
 <html>
 <head>
 	<title>JobLister</title>
-	<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
-  <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
-
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+	<link rel="stylesheet" href="css/styles.css">
 </head>
 <body>
 	<div class="container">
       <div class="header clearfix">
         <nav>
-          <ul class="nav nav-pills pull-right">
-            <li role="presentation"><a href="index.php">Home</a></li>
-            <li role="presentation"><a href="create.php">Create Listing</a></li>
-          </ul>
+					<ul class="nav justify-content-center">
+					  <li class="nav-item">
+					    <a class="nav-link active" href="index.php">Home</a>
+					  </li>
+					  <li class="nav-item">
+					    <a class="nav-link active" href="create.php">New Listing</a>
+					  </li>
+
+					</ul>
         </nav>
         <h3 class="text-muted"><?php echo SITE_TITLE; ?></h3>
       </div>
